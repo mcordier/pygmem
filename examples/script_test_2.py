656d9697fe4c8bd5bb1cbb027c54c766fb0a9b05
mcordier <kanasukre@gmail.com>
2021-09-30 10:02:02 +0200

    0.2 version commit

---
+++ examples/script_test_2.py
@@ -0,0 +1,59 @@
+"""
+==========================================================================
+ratWeight Example: Introduction to Generalized Mixed Effect Models
+==========================================================================
+
+In general, Mixed Effect Models try to learn fixed effect parameters, like in 
+a regular regression, and some random effect parameters derived from a group
+structure. There are a vector parameter per group category to learn, which makes
+usually the computation of those parameters more difficult. From a statistical
+point of view, we could be also interested in the variance of the parameters 
+regarding the group population, but in our context, we only focus on prediction
+and machine learning task, so we will only be interested in a fast and realiable
+prediction from ou models.
+
+This example illustrates both linear and non linear generalized mixed effect
+models in the example of ratWeight dataset. The dependant variable is
+the weight and the independent variables are the number of weeks.
+Each rat is unique and are identified with the ids. Then, it is difficult 
+to get a general prediction and that is where the GMEM becomes interesting. 
+We will use the default link function (identity) because this is a regression
+(continuous domain)
+
+First, we print for 2 group categories the evolution of concentration  with time
+and illustrates the individualisation of prediction.
+
+Then, we fit a linear mixed effect model with 1 fixed variables (week)
+and 1 random effect variable (week). We see that the linear hypothesis is not
+suited.
+
+Then, we fit a non linear mixed effect model with 1 fixed variables (week)
+and 1 random effect variable (week). Here, with only variable, we try to predict
+individualized predictions. We set an encoder architecture for vectors
+transformation and the random and fixed effect are learned in the layer of the
+middle, which enables the maximum of flexibility (low bias) in the learning pro-
+cess.
+
+"""
+print(__doc__)
+
+
+
+import numpy as np
+import pandas as pd
+import torch
+from pygmem.gmem import GMEM
+
+df = pd.read_csv('data/ratWeight.csv')
+df['id'] = df['id'].astype('category')
+df[['id']]  = df[['id']].apply(lambda x: x.cat.codes)
+
+# Linear Model
+model = GMEM(1, 1, max(np.unique(df.id.values))+1) #, theta=torch.exp
+model.fit(df, ['week'], ['week'], df.id.values, 'weight', visualize=True)
+
+# Non Linear Model
+model = GMEM(1, 1, max(np.unique(df.id.values))+1,
+             enc_in=[1, 4], enc_out=[4, 30, 1])  # theta=torch.exp
+model.fit(df, ['week'], ['week'], df.id.values, 'concentration',
+          lr_f=0.005, visualize=True)
--- pygmem/dataset.py
+++ pygmem/dataset.py
@@ -1,63 +1,34 @@
-import os
 import torch
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader, Sampler
-import random
 from tqdm import tqdm
-import math
 
-class Dataset_simulated(Dataset):
-    """
-    """
-    def __init__(self, n_features, X, Z, group_ids, n_samples=100, transform=None, nl_fun=None):
-        """
-        """
-        self.group_index = np.unique(group_ids)
-        self.data = np.random.normal(-0.5, 0.2, size=(n_samples, n_features))
-        self.x = self.data[:, X]
-        self.z = self.data[:, Z] # decide poisson theta values
-        print(self.x.shape, self.z.shape)
-        self.linear_f = np.random.randint(0, 1, size=len(X))-3
-        self.linear_m = np.random.random(size=(len(Z), len(self.group_index)))*2-1
-        # print(self.linear_f, self.linear_m)
-        self.y = np.zeros((n_samples))
-        self.y_hat = np.zeros((n_samples))
-        print(self.y.shape)
-
-        for i in self.group_index:
-
-            if nl_fun is not None:
-                self.y[np.where(group_ids==i)] = nl_fun(self.x[np.where(group_ids==i), :][0].dot(self.linear_f) + 1)
-                self.y[np.where(group_ids==i)] += nl_fun(self.z[np.where(group_ids==i)][0].dot(self.linear_m[:,i]))
-                print(self.y[np.where(group_ids==i)])
-            else:
-                self.y[np.where(group_ids==i)] = self.x[np.where(group_ids==i), :][0].dot(self.linear_f) + 1
-                self.y[np.where(group_ids==i)] += self.z[np.where(group_ids==i)][0].dot(self.linear_m[:,i])
-            self.y_hat[np.where(group_ids==i)] = np.exp(np.cos(self.y[np.where(group_ids==i)]))
-            self.y[np.where(group_ids==i)] = np.random.poisson(self.y_hat[np.where(group_ids==i)])
-
-        self.y = torch.tensor(self.y,  dtype=torch.float32).unsqueeze(-1)
-        self.x = torch.tensor(self.x, dtype=torch.float32)
-        self.z = torch.tensor(self.z, dtype=torch.float32)
-        self.group_ids = torch.tensor(group_ids, dtype=torch.long)
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        sample = {'x': self.x[idx], 'y':self.y[idx], 
-            'z': self.z[idx], 'group_ids': self.group_ids[idx]}
-
-        if self.transform:
-            sample = self.transform(sample)
-
-        return sample
 
 class Dataset_gmem(Dataset):
     """
+    Customized Dataset class for pytorch data ingestion
+
+    Args:
+    ----------
+        data : pandas.DataFrame or array-like
+
+        X : [str]
+            column names for fixed effects
+
+        Z : [str]
+            column names for fixed effects
+
+        group_ids : [int]
+            list of ids for the group index (no multi-groups)
+
+        n_samples : int
+
+        nl_fun : function
+
+    Attributes
+    ----------
+
     """
     def __init__(self, data, X, Z, groups, y, transform=None):
         """
@@ -76,10 +47,6 @@ class Dataset_gmem(Dataset):
             self.y = self.data[:, y]
         else:
             raise Exception('not good data type')
-        # print(self.x.shape, self.z.shape)
-        # print(self.linear_f, self.linear_m)
-        print(self.y.shape)
-
         self.y = torch.tensor(self.y,  dtype=torch.float32).unsqueeze(-1)
         self.x = torch.tensor(self.x, dtype=torch.float32)
         self.z = torch.tensor(self.z, dtype=torch.float32)
@@ -90,24 +57,85 @@ class Dataset_gmem(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        sample = {'x': self.x[idx], 'y':self.y[idx], 
-            'z': self.z[idx], 'group_ids': self.group_ids[idx]}
+        sample = {'x': self.x[idx], 'y': self.y[idx],
+                  'z': self.z[idx], 'group_ids': self.group_ids[idx]}
 
         if self.transform:
             sample = self.transform(sample)
 
         return sample
 
-# df = pd.read_csv('data/warfarinData.csv') #theophyllineData
-# print(df)
-# dataset = Dataset(df, ['time', 'weight'], ['time'], df.id.values, 'concentration')
 
-# N =1000
-# groups = np.random.randint(0, 5, size=N)
-# dataset = Dataset_simulated(1, [0], [0], groups, n_samples=N)
-# for i in groups:
-#     plt.scatter(dataset.x[np.where(groups==i), :], dataset.y[np.where(groups==i)])
-# plt.show()
+class Dataset_simulated(Dataset):
+    """
+    Customized Dataset class for simulated data (Poission) To be changed/erased
+    soon.
+
+    Args:
+    ----------
+        n_features : int
+
+        X : int
+
+        Z : int
+
+        group_ids : list
+
+        n_samples : int
+
+        nl_fun : function
+    """
+    def __init__(
+            self, n_features, X, Z, group_ids,
+            n_samples=100, transform=None, nl_fun=None):
+        """
+        """
+        self.group_index = np.unique(group_ids)
+        self.data = np.random.normal(-0.5, 0.2, size=(n_samples, n_features))
+        self.x = self.data[:, X]
+        self.z = self.data[:, Z]  # decide poisson theta values
+        self.linear_f = np.random.randint(0, 1, size=len(X))-3
+        self.linear_m = np.random.random(
+            size=(len(Z), len(self.group_index)))*2-1
+        self.y = np.zeros((n_samples))
+        self.y_hat = np.zeros((n_samples))
+
+        for i in self.group_index:
+            if nl_fun is not None:
+                self.y[np.where(group_ids == i)] = nl_fun(
+                    self.x[np.where(group_ids == i), :][0].dot(
+                        self.linear_f) + 1)
+                self.y[np.where(group_ids == i)] += nl_fun(
+                    self.z[np.where(group_ids == i)][0].dot(
+                        self.linear_m[:, i]))
+            else:
+                self.y[np.where(group_ids == i)] = self.x[np.where(
+                    group_ids == i), :][0].dot(self.linear_f) + 1
+                self.y[np.where(
+                    group_ids == i)] += self.z[np.where(
+                        group_ids == i)][0].dot(self.linear_m[:, i])
+            self.y_hat[np.where(group_ids == i)] = np.exp(
+                np.cos(self.y[np.where(group_ids == i)]))
+            self.y[np.where(group_ids == i)] = np.random.poisson(
+                self.y_hat[np.where(group_ids == i)])
+
+        self.y = torch.tensor(self.y,  dtype=torch.float32).unsqueeze(-1)
+        self.x = torch.tensor(self.x, dtype=torch.float32)
+        self.z = torch.tensor(self.z, dtype=torch.float32)
+        self.group_ids = torch.tensor(group_ids, dtype=torch.long)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sample = {'x': self.x[idx], 'y': self.y[idx],
+                  'z': self.z[idx], 'group_ids': self.group_ids[idx]}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
 
 
 class BatchGroupSampler(Sampler):
@@ -150,21 +178,28 @@ class BatchGroupSampler_fast(Sampler):
 
 def generate_dataloader_indexed(dataset):
     '''
-    Generate e compatible fast pytorch dataloader from a pytorch popularity_dataset.
-    Th sampler constructs batches with same userIds (same rows, same OID/OR/DEST)
-    arg : PopularityDataset
-    return : Pytorch Dataloader
+    Generate a compatible fast pytorch dataloader from a pytorch
+    popularity_dataset. The sampler constructs batches with same userIds
+    (same rows, same OID/OR/DEST)
+
+    Args:
+    ----------
+    dataset : Dataset
+
+    returns:
+    ----------
+    dataloader_idx : Dataloader
+        Dataloader for random effects with indexing in the right order
     '''
 
     # Train loaders by batch of same iDs
     userIds_list = np.array(dataset.group_ids)
     userIds_uniques = np.array(dataset.group_ids.unique())
-    print(userIds_list)
-    print(userIds_uniques)
 
     # Create a list of the index (userId) of each value for the sampler
     group_index = np.empty((userIds_uniques.max()+1,), dtype=object)
-    for (i, user) in enumerate(tqdm(userIds_list)): #list of list of indexes per Ids_unique
+    # list of list of indexes per Ids_unique
+    for (i, user) in enumerate(tqdm(userIds_list)):
         if group_index[user] == None:
             group_index[user] = [i]
         else:
@@ -174,6 +209,5 @@ def generate_dataloader_indexed(dataset):
     sampler = BatchGroupSampler_fast(group_index, userIds_uniques)
     # sampler = BatchGroupSampler(userIds_list, userIds_uniques)
     dataloader_idx = DataLoader(dataset, batch_sampler=sampler,
-                            num_workers=0)
-
+                                num_workers=0)
     return(dataloader_idx)
